2186424581f5b1ced8142fe085355aa2acb6c62e
AKSHAY-2452 <67816894+AKSHAY-2452@users.noreply.github.com>
2022-08-20 20:05:53 +0530

    Time: 59 ms (90.79%), Space: 10.3 MB (39.04%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -10,15 +10,35 @@ class Solution {
         }
         return dp[i][j] = mini;
     }
-public:
-    int minCost(int n, vector<int>& cuts) {
+    
+    int solveTab(int n, vector<int>& cuts){
         cuts.push_back(n);
         cuts.insert(cuts.begin(),0);
-        
         sort(cuts.begin(),cuts.end());
+        vector<vector<int>> dp(cuts.size(),vector<int>(cuts.size(),0)); //base case already covered
         
-        int z = cuts.size()-2;
-        vector<vector<int>> dp(cuts.size(),vector<int>(cuts.size(),-1));
-        return solve(cuts,1,z,dp);
+        for(int i = cuts.size()-1;i>=1;i--){
+            for(int j = i;j<=cuts.size()-2;j++){
+                int mini = 1e9;
+                for(int k = i;k<=j;k++){
+                    int temp = (cuts[j+1] - cuts[i-1]) + dp[i][k-1] + dp[k+1][j];
+                    mini = min(mini,temp);
+                }
+                dp[i][j] = mini;
+            }
+        }
+        return dp[1][cuts.size()-2];
+    }
+public:
+    int minCost(int n, vector<int>& cuts) {
+        //cuts.push_back(n);
+        //cuts.insert(cuts.begin(),0);
+        //sort(cuts.begin(),cuts.end());
+        
+        //int z = cuts.size()-2;
+        //vector<vector<int>> dp(cuts.size(),vector<int>(cuts.size(),-1));
+        //return solve(cuts,1,z,dp);
+        
+        return solveTab(n,cuts);
     }
 };
